3002f853d4ac052a92a7ccef2647ba8527bf93b1
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-04-27 17:11:01 -0400

    Bump to v1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'all_rights'
 description      'Installs/Configures elk_forwarder'
 long_description 'Installs/Configures elk_forwarder'
-version          '0.0.1'
+version          '1.0.0'
 
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 7'
