170d1b683ab89ef364ae53bf72737c6125ae4ac0
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-04-27 14:55:44 -0400

    Add Notifies to elk_forwarder_log

--- providers/log.rb
+++ providers/log.rb
@@ -4,11 +4,16 @@ action :track do
     paths: new_resource.paths,
     fields: new_resource.fields
   }
+  before = node['elk_forwarder']['config']['files'].to_json
+
   node.set['elk_forwarder']['config']['files'] = node['elk_forwarder']['config']['files'].merge(new_log)
+
+  new_resource.updated_by_last_action(true) if before != node['elk_forwarder']['config']['files'].to_json
 end
 
 action :untrack do
   node.set['elk_forwarder']['config']['files'] = node['elk_forwarder']['config']['files'].select do |f|
+    new_resource.updated_by_last_action(true) unless f.name == new_resource.name
     f.name == new_resource.name
   end
 end
