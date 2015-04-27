a602070750a09a12fe9343c034d7052b19836c60
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-04-27 17:06:15 -0400

    Build in a separate dir and copy binrary to install path

--- attributes/_source.rb
+++ attributes/_source.rb
@@ -1,2 +1,3 @@
 default['elk_forwarder']['git_repo'] = 'https://github.com/elastic/logstash-forwarder'
 default['elk_forwarder']['git_ref'] = 'v0.4.0'
+default['elk_forwarder']['build_dir'] = '/tmp/logstash-forwarder-build-dir'
--- recipes/_source.rb
+++ recipes/_source.rb
@@ -8,27 +8,42 @@
 
 include_recipe 'golang'
 
-directory node['elk_forwarder']['install_dir'] do
+# Enture that our install and build directories exist
+directory "#{node['elk_forwarder']['install_dir']}/bin" do
   user node['elk_forwarder']['user']
   group node['elk_forwarder']['group']
   mode 0700
+  recursive true
 end
 
-git node['elk_forwarder']['install_dir'] do
+directory node['elk_forwarder']['build_dir'] do
+  recursive true
+end
+
+git node['elk_forwarder']['build_dir'] do
   repository node['elk_forwarder']['git_repo']
   revision node['elk_forwarder']['git_ref']
   action :checkout
-  notifies :run, 'execute[build-logstash-forwarder]'
+  notifies :run, 'execute[build-logstash-forwarder]', :immediately
 end
 
+# Build the logstash forwarder
 execute 'build-logstash-forwarder' do
-  cwd node['elk_forwarder']['install_dir']
-  command '/usr/local/go/bin/go build'
+  cwd node['elk_forwarder']['build_dir']
+  command '/usr/local/go/bin/go build -o logstash-forwarder'
   action :nothing
+end
+
+# Install the actual binary
+remote_file 'Copy logstash-forwarder binary' do
+  path "#{node['elk_forwarder']['install_dir']}/bin/logstash-forwarder"
+  source "file://#{node['elk_forwarder']['build_dir']}/logstash-forwarder"
   user node['elk_forwarder']['user']
   group node['elk_forwarder']['group']
+  mode 0755
 end
 
+# Add the service file
 template "/etc/init.d/#{node['elk_forwarder']['service_name']}" do
   source 'logstash-forwarder-init.erb'
   owner 'root'
@@ -36,6 +51,7 @@ template "/etc/init.d/#{node['elk_forwarder']['service_name']}" do
   mode 0755
 end
 
+# Enable and start the service
 service node['elk_forwarder']['service_name'] do
   supports status: true, restart: true
   action [:enable, :start]
