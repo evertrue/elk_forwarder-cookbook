b85b37ca2548250da7b4713f2a16f051db9766c4
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-18 14:15:39 -0400

    remove :untrack action and fix array merging in log provider

--- providers/log.rb
+++ providers/log.rb
@@ -6,18 +6,10 @@ action :track do
   }
   before = node['elk_forwarder']['config']['files'].to_json
 
-  files = node['elk_forwarder']['config']['files'].merge(new_log)
+  files = (node['elk_forwarder']['config']['files'] + [new_log]).uniq
   node.set['elk_forwarder']['config']['files'] = files
 
   if before != node['elk_forwarder']['config']['files'].to_json
     new_resource.updated_by_last_action(true)
   end
-end
-
-action :untrack do
-  files = node['elk_forwarder']['config']['files'].select do |f|
-    new_resource.updated_by_last_action(true) unless f.name == new_resource.name
-    f.name == new_resource.name
-  end
-  node.set['elk_forwarder']['config']['files'] = files
 end
