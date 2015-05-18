8a23742dc84975bf9af01df7707beb3448644edf
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-18 14:14:03 -0400

    Add optional certs Recipe for configuring lumberjack certificates

---
+++ attributes/certs.rb
@@ -0,0 +1,3 @@
+default['elk_forwarder']['certs']['ca_data_bag'] = 'certificates'
+default['elk_forwarder']['certs']['ca_data_bag_item'] = 'logstash'
+default['elk_forwarder']['certs']['ca_data_bag_item_key'] = 'ca certificate'
---
+++ recipes/certs.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: elk_forwarder
+# Recipe::certs
+#
+# This is an optional recipe for installing and configuring the ssl settings
+# Logstash Forwarder.  It Searches in specified data bag items for the
+# ca, cert, and keys.
+
+%w(ca key certificate).each do |c|
+  unless node['elk_forwarder']['certs']["#{c}_data_bag"]
+    node.rm('elk_forwarder', 'config', 'network', "ssl #{c}")
+    next
+  end
+
+  ssl_thing = data_bag_item(
+    node['elk_forwarder']['certs']["#{c}_data_bag"],
+    node['elk_forwarder']['certs']["#{c}_data_bag_item"]
+  )[node['elk_forwarder']['certs']["#{c}_data_bag_item_key"]]
+
+  directory File.dirname(node['elk_forwarder']['config']['network']["ssl #{c}"]) do
+    recursive true
+  end
+
+  file node['elk_forwarder']['config']['network']["ssl #{c}"] do
+    content ssl_thing
+    not_if { ssl_thing.nil? || ssl_thing.empty? }
+  end
+end
