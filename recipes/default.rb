b97d830a93666cf328d3bbf422a4eba5e55d7e29
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-18 14:18:41 -0400

    If we are mocking (test) nodes then add the hosts entry for connecting to the elk_logstash node

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,3 +12,9 @@ when 'source'
 end
 
 include_recipe 'elk_forwarder::configure'
+
+file '/etc/hosts' do
+  content IO.read('/etc/hosts') + "\n192.168.33.10\t_default-logstash.priv.evertrue.com"
+  only_if { node['elk_forwarder']['mocking'] }
+  not_if { IO.read('/etc/hosts').include? '_default-logstash' }
+end
