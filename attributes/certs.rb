5a37b6cb7fb96fcb8ce5dd17f97b7f864579ae7f
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-16 12:03:25 -0400

    Remove default attributes for the certs recipe and instead provide examples

--- attributes/certs.rb
+++ attributes/certs.rb
@@ -1,3 +1,4 @@
-default['elk_forwarder']['certs']['ca_data_bag'] = 'certificates'
-default['elk_forwarder']['certs']['ca_data_bag_item'] = 'logstash'
-default['elk_forwarder']['certs']['ca_data_bag_item_key'] = 'ca certificate'
+# Examples of how to specify the paths to data bag items
+# default['elk_forwarder']['certs']['ca_data_bag'] = 'certificates'
+# default['elk_forwarder']['certs']['ca_data_bag_item'] = 'logstash'
+# default['elk_forwarder']['certs']['ca_data_bag_item_key'] = 'ca certificate'
