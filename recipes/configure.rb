2536403aac6ad8237f2c3e8be63fc246b647e19f
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-16 18:06:03 -0400

    Convert the files hash to the array that it needs to be and then run the template

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -26,16 +26,30 @@ directory node['elk_forwarder']['config_dir'] do
   action :create
 end
 
+ruby_block 'forwarder_attribute_manipulation' do
+  block do
+    require 'json'
+    final = JSON.parse(node['elk_forwarder']['config'].to_json)
+    config = JSON.parse(node['elk_forwarder']['config'].to_json)
+    final['files'] = []
+    config['files'].each do |_key, value|
+      final['files'] << value
+    end
+    node.set['elk_forwarder']['config'] = nil # Wipe it clean
+    node.set['elk_forwarder']['config'] = final # set it
+    Chef::Log.warn node['elk_forwarder']['config'].to_json
+  end
+  notifies :create, 'template[Logstash Forwarder Configuration]', :immediately
+end
+
 # Generate the config file using the config attribute
-template 'Logstash Forwarder Base Configuration' do
+template 'Logstash Forwarder Configuration' do
   cookbook 'elk_forwarder'
   path "#{node['elk_forwarder']['config_dir']}/logstash-forwarder.conf"
   source 'logstash-forwarder.conf.erb'
   user node['elk_forwarder']['user']
   group node['elk_forwarder']['group']
   mode '0640'
-  variables(
-    config: JSON.parse(node['elk_forwarder']['config'].to_json)
-  )
+  action :nothing
   notifies :restart, "service[#{node['elk_forwarder']['service_name']}]"
 end
