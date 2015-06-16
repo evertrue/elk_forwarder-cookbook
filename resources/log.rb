666e0f4960b233c8f7d1e9282bb131de11d1385e
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-16 18:04:53 -0400

    Remove the LWRP as is only complicates things unfortunately

--- resources/log.rb
+++
@@ -1,6 +0,0 @@
-actions :track, :untrack
-default_action :track
-
-attribute :name,           kind_of: String, name_attribute: true
-attribute :paths,          kind_of: Array
-attribute :fields,         kind_of: Hash
