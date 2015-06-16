1d818482a81f8e1c9d01c64c47a0060a6ec30129
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-16 14:20:03 -0400

    Bump to v1.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'apache2'
 description      'Installs logstash-forwarder for use in an ELK Cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.3'
+version          '1.0.4'
 
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 7'
