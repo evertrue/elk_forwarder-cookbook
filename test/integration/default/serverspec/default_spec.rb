80b6ddc77c754635083e919db23e0956dc1e540a
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-06-17 13:00:11 -0400

    Add tests for the certificates

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,12 +10,18 @@ describe 'It installs logstash-forwarder' do
 
   describe file '/etc/logstash-forwarder/logstash-forwarder.conf' do
     it { is_expected.to be_file }
-    its(:content) { is_expected.to include '"ssl ca":"/' }
+    its(:content) { is_expected.to include '"ssl ca":' }
+    its(:content) { is_expected.to include '/etc/pki/tls/certs/logstash-forwarder/ca.pem' }
     its(:content) { is_expected.to include '/var/log/syslog' }
   end
 
   describe file '/var/log/logstash-forwarder/logstash-forwarder.log' do
     it { is_expected.to be_file }
+  end
+
+  describe file '/etc/pki/tls/certs/logstash-forwarder/ca.pem' do
+    it { is_expected.to be_file }
+    its(:content) { is_expected.to include 'MIIHSzCCBTOgAwIBAgIJAOs1IogcrEX7MA0GCSq' }
   end
 
   describe service('logstash-forwarder') do
