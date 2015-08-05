6505574caa6d72a8128fd2a14000cf5aa574627e
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-08-04 23:37:51 -0400

    Set mode for SSL Related objects to 600

--- recipes/certs.rb
+++ recipes/certs.rb
@@ -25,6 +25,7 @@
 
   file node['elk_forwarder']['config']['network']["ssl #{c}"] do
     content ssl_thing
+    mode '600'
     not_if { ssl_thing.nil? || ssl_thing.empty? }
     notifies :restart, 'service[logstash-forwarder]'
   end
