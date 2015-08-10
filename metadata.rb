36e5c7f5ecec108245ca9b941bdca1ab800c9c49
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-08-10 13:05:31 -0400

    Update supports clauses in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,7 @@ description      'Installs logstash-forwarder for use in an ELK Cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.1'
 
-supports 'ubuntu', '>= 12.04'
-supports 'centos', '>= 7'
+supports 'ubuntu', '>= 14.04'
 
 depends 'golang', '~> 1.5'
 depends 'logrotate', '~> 1.9'
