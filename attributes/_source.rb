a3c25279a327dfa4f2b61e6ff0ef0082ed46f029
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-11-04 09:11:46 -0500

    Change default build dir to be in file_cache_path

--- attributes/_source.rb
+++ attributes/_source.rb
@@ -1,3 +1,3 @@
 default['elk_forwarder']['git_repo'] = 'https://github.com/elastic/logstash-forwarder'
 default['elk_forwarder']['git_ref'] = 'v0.4.0'
-default['elk_forwarder']['build_dir'] = '/tmp/logstash-forwarder-build-dir'
+default['elk_forwarder']['build_dir'] = "#{Chef::Config[:file_cache_path]}/logstash-forwarder-build"
