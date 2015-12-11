bc72820f234e957e8c1203c31a2e9dbbcbd520d2
Eric Herot <eric.github@herot.com>
2015-12-11 15:35:54 -0500

    Add not_if File.exist? to openssl_x509 as workaround to notification bug

--- recipes/certs.rb
+++ recipes/certs.rb
@@ -28,6 +28,7 @@ if node['elk_forwarder']['generate_cert']
       key_file node['elk_forwarder']['ca_cert']['self_signed']['signing_key']
     end
     notifies :restart, 'service[logstash-forwarder]'
+    not_if { ::File.exist? ca_cert } # Necessary because of a bug in openssl_x509
   end
 else
   file ca_cert do
